42caf04acd56c315f3d661d81de765cf0ce26dc8
Muhammad Salah <47748059+MohamedSalahApdElzaher@users.noreply.github.com>
2021-03-21 02:30:40 +0200

    Update main.py

--- Intermediate Projects/Quiz App/main.py
+++ Intermediate Projects/Quiz App/main.py
@@ -2,7 +2,7 @@ from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
 
-question_bank = []
+question_bank = [] 
 
 for question in question_data:
     ques = Question(question["text"], question["answer"])
